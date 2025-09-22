004375561992c05d30bc4698af7acc2aaebf3af5
Sohaibkundi2 <sohaibkundi2@gmail.com>
2025-09-22 22:13:08 +0500

    add Sieve of Eratosthenes implementation

--- Week-01/Day-04_Basic-Math.js
+++ Week-01/Day-04_Basic-Math.js
@@ -1,24 +1,53 @@
-// PRIME NUMBER CHECK
-let n = 7;
+function isPrime() {
+  // PRIME NUMBER CHECK
+let n =56;
+
 let isPrime = true;
 
-if (n < 2) {
+if(n <2){
   isPrime = false;
-} else {
-  for (let i = 2; i * i <= n; i++) {
-    if (n % i === 0) {
-      isPrime = false;
-      break;
-    }
+}
+
+for(let x=2; x*x <=n;x++){
+  if(n % x === 0){
+    isPrime = false;
+    break;
   }
 }
 
-if (isPrime) {
-  console.log(n + " is Prime");
-} else {
-  console.log(n + " is Not Prime");
+if(isPrime){
+  console.log(n, " is Prime")
+}else{
+  console.log(n, " not prime")
 }
-
 // here 
 // Time Complexity = O(√n)
 // Auxiliary Space = O(1)
+}
+//******************************************************************************************************
+
+// SIEVE OF ERATOSTHENES
+function sieveOfEratosthenes(n) {
+  let isPrime = new Array(n + 1).fill(true);
+  isPrime[0] = isPrime[1] = false; // 0 and 1 are not prime numbers
+
+  for (let p = 2; p * p <= n; p++) {
+    if (isPrime[p]) {
+      for (let multiple = p * p; multiple <= n; multiple += p) {
+        isPrime[multiple] = false;
+      }
+    }
+  }
+
+  // collect all primes
+  let primes = [];
+  for (let i = 2; i <= n; i++) {
+    if (isPrime[i]) primes.push(i);
+  }
+
+  return primes;
+}
+
+// Example:
+let n = 50;
+console.log("Prime numbers up to", n, "are:", sieveOfEratosthenes(n));
